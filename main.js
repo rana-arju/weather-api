5de64d0a7516b92aac877dd2cbc6b7bcfb6491a6
Rana Arju <arjurana530@gmail.com>
2022-03-03 20:34:43 +0600

    after complate api project

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const searchTem = () => {
     const cityName = document.getElementById("city_name").value;
+    document.getElementById("city_name").value ="";
     const Api_KEY = `f3f9682307a5301fed413db0ea0ab714`;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${Api_KEY}&units=metric`)
     .then(response => response.json())
@@ -8,5 +9,9 @@ const searchTem = () => {
 const displayData = data => {
     document.getElementById("city").innerText = data.name;
     document.getElementById("tem").innerText = data.main.temp;
+    document.getElementById("condition").innerText = data.weather[0].main;
+    const imgUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById("weather-img");
+    imgIcon.setAttribute("src", imgUrl);
     console.log(data);
 }
